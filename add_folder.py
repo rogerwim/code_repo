f8edc072a3a869dcbf0e170b468b3b625bf90f3a
Roger van Bommel <f_vanbommel@yahoo.co.uk>
2021-04-24 14:45:00 +0200

    add glob

--- add_folder.py
+++ add_folder.py
@@ -1,20 +1,6 @@
 import os
 import add_file
-def getListOfFiles(dirName):
-    # create a list of file and sub directories 
-    # names in the given directory 
-    listOfFile = os.listdir(dirName)
-    allFiles = list()
-    # Iterate over all the entries
-    for entry in listOfFile:
-        # Create full path
-        fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            allFiles.append(fullPath)
-                
-    return allFiles
-for file in getListOfFiles("C:\\Users\\Gert\\Pictures\OUDE_LUMIX"):
+import glob
+import sys
+for file in glob.glob(os.path.abspath(os.path.expanduser(sys.argv[1]))+"/**",recursive=True):
     add_file.add("test1.py",file)
