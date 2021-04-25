4efa90ec161a6845ab61e003dea2aa6621e7b5ec
Roger van Bommel <f_vanbommel@yahoo.co.uk>
2021-04-25 09:09:58 +0200

    test

--- add_folder.py
+++ add_folder.py
@@ -2,5 +2,5 @@ import os
 import add_file
 import glob
 import sys
-for file in [f for f in glob.glob(os.path.abspath(os.path.expanduser("../backup"))+"/**/*",recursive=True) if os.path.isfile(f)]:
+for file in [f for f in glob.glob(os.path.abspath(os.path.expanduser(sys.argv[1]))+"/**/*",recursive=True) if os.path.isfile(f)]:
     add_file.add("test1.py",file)
