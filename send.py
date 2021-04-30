17e01c57b7d7ed6d92759a33e0bc35079669123f
Your Name <you@example.com>
2021-04-30 09:11:43 +0200

    add encryption

--- send.py
+++ send.py
@@ -8,18 +8,12 @@ import zlib
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
-key = RSA.generate(2048)
+file = open('private.pem','rb')
+key_exp = file.read()
+file.close
+key = RSA.import_key(key_exp)
 private_key = key.export_key()
-file_out = open("private.pem", "wb")
-file_out.write(private_key)
-file_out.close()
-
 public_key = key.publickey().export_key()
-file_out = open("receiver.pem", "wb")
-file_out.write(public_key)
-file_out.close()
-
-
 
 
 
